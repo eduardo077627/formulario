d13eb47245d57cccc7c89dfd6ad1822e4460de85
eduardo077627 <varmuth.silva@escola.pr.gov.br>
2025-09-08 12:02:45 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
+  alert("oi amigo!"); 
 } 
